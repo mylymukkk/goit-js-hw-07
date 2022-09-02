4ad432a717f1a6c0ef62288070f39bac578252f9
Mylymuk Oksana <oksana.mylymukk@gmail.com>
2022-09-03 00:42:09 +0300

    finished task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const photos = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`
+  )
+  .join("");
+
+gallery.innerHTML = photos;
+
+let modal;
+
+const zoomPhoto = (event) => {
+  event.preventDefault();
+
+  const selectedPhoto = event.target.dataset.source;
+
+  modal = basicLightbox.create(`
+   <img class="gallery__image" src="${selectedPhoto}"/>`);
+
+  modal.show();
+
+  gallery.addEventListener("keydown", closePhoto);
+};
+
+const closePhoto = (event) => {
+  if (event.code === "Escape") {
+    modal.close();
+    gallery.removeEventListener("keydown", closePhoto);
+  }
+};
+
+gallery.addEventListener("click", zoomPhoto);
